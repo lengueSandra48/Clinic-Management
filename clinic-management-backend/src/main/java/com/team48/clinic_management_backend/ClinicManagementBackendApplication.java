5c4836c62384e57359615559690ff3585006a621
Sandra lengue <lenguesandra@gmail.com>
2025-05-29 14:05:05 +0100

    add the backend folder

---
+++ clinic-management-backend/src/main/java/com/team48/clinic_management_backend/ClinicManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.team48.clinic_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicManagementBackendApplication.class, args);
+	}
+
+}
---
+++ clinic-management-backend/src/test/java/com/team48/clinic_management_backend/ClinicManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team48.clinic_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
